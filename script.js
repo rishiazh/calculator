f33d9686030d10743477fe15ecc43aaa1096aac9
RISHI MADDHESHIYA <126286231+rishiazh@users.noreply.github.com>
2023-08-08 02:36:53 +0530

    Add files via upload
    In this calculator there is some another feature liks change background like dark light.

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
-function inputGiven(e){
-    let target = e.target;
-    let container = document.getElementsByClassName('btn-container')[0];
-    if(target===container){
-    
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
     }
-    else{
-    inpGiven = target.innerText;
-    var inp = document.getElementById('inp');
-   if(inpGiven ===  'del'){
-       if(inp.value === 'Invalid Expression'){
-           inp.value='';
-       }
-       else{
-           inp.value=inp.value.slice(0,inp.value.length-1);
-       }
-   }
-   if(inpGiven === '='){
-       try {
-           inp.value= eval(inp.value);
-       } catch (error) {
-           inp.value = 'Invalid Expression';  
-       }
-   }
-   else if (inpGiven !=  'del'){
-       inp.value = inp.value + inpGiven;
-   }
-   if(inpGiven==='C'|| inp.value==='C'){
-       inp.value='';
-   } 
-   }
-   }
-   let btnContainer = document.getElementsByClassName('btn-container')[0];
-   btnContainer.addEventListener('click',inputGiven);
+  };
+});
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
